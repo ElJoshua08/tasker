7158ca83594d928737703768bbf1beabb6df1c52
Joshua <josuealejandrof926@gmail.com>
2024-10-21 00:10:49 +0200

    register with oauth and google

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { GoogleIcon } from '@/components/google-icon';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -18,6 +19,7 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
+import { signUpWithGoogle } from '@/lib/server/oauth';
 import { register } from '@/services/auth.service';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { EyeIcon, EyeOffIcon, LoaderCircleIcon } from 'lucide-react';
@@ -65,112 +67,131 @@ export default function RegisterPage() {
   };
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="font-medium text-2xl uppercase tracking-widest">
-          Register
-        </CardTitle>
-        <Separator />
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form className="space-y-6">
-            <FormField
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="username">Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="text"
-                      placeholder="Tell us your name"
-                      autoFocus
-                      id="username"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="email"
-                      autoComplete="email"
-                      id="email"
-                      placeholder="Enter your email"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              name="password"
-              render={({ field }) => {
-                return (
+    <section className="flex flex-col items-center justify-center gap-4 w-72">
+      <Card className='w-full'>
+        <CardHeader>
+          <CardTitle className="font-medium text-2xl uppercase tracking-widest">
+            Register
+          </CardTitle>
+          <Separator />
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form className="space-y-6">
+              <FormField
+                name="username"
+                render={({ field }) => (
                   <FormItem>
-                    <FormLabel htmlFor="password">Password</FormLabel>
-                    <div className="relative">
-                      <FormControl>
-                        <Input
-                          type={showPassword ? 'text' : 'password'}
-                          autoComplete="current-password"
-                          id="password"
-                          placeholder="Enter your password"
-                          {...field}
-                        />
-                      </FormControl>
-                      {showPassword ? (
-                        <EyeIcon
-                          className="absolute right-4  top-1/2 -translate-y-1/2 text-foreground/60 cursor-pointer"
-                          size={20}
-                          onClick={() => setShowPassword(false)}
-                        />
-                      ) : (
-                        <EyeOffIcon
-                          className="absolute right-4  top-1/2 -translate-y-1/2 text-foreground/60 cursor-pointer"
-                          size={20}
-                          onClick={() => setShowPassword(true)}
-                        />
-                      )}
-                    </div>
+                    <FormLabel htmlFor="username">Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Tell us your name"
+                        autoFocus
+                        id="username"
+                        {...field}
+                      />
+                    </FormControl>
                     <FormMessage />
                   </FormItem>
-                );
-              }}
-            />
-          </form>
-        </Form>
-      </CardContent>
-      <CardFooter className="flex items-end justify-between w-full gap-8">
-        <Link href="/login">
+                )}
+              />
+
+              <FormField
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        autoComplete="email"
+                        id="email"
+                        placeholder="Enter your email"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                name="password"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel htmlFor="password">Password</FormLabel>
+                      <div className="relative">
+                        <FormControl>
+                          <Input
+                            type={showPassword ? 'text' : 'password'}
+                            autoComplete="current-password"
+                            id="password"
+                            placeholder="Enter your password"
+                            {...field}
+                          />
+                        </FormControl>
+                        {showPassword ? (
+                          <EyeIcon
+                            className="absolute right-4  top-1/2 -translate-y-1/2 text-foreground/60 cursor-pointer"
+                            size={20}
+                            onClick={() => setShowPassword(false)}
+                          />
+                        ) : (
+                          <EyeOffIcon
+                            className="absolute right-4  top-1/2 -translate-y-1/2 text-foreground/60 cursor-pointer"
+                            size={20}
+                            onClick={() => setShowPassword(true)}
+                          />
+                        )}
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="flex items-end justify-between w-full gap-8">
           <Button
-            variant="link"
-            className="flex items-end p-0"
+            disabled={form.formState.isSubmitting}
+            className="flex items-center gap-x-2 w-full"
+            onClick={form.handleSubmit(onSubmit)}
           >
-            Already have an account?
+            Register
+            {form.formState.isSubmitting && (
+              <LoaderCircleIcon className="animate-spin" />
+            )}
           </Button>
-        </Link>
+        </CardFooter>
+      </Card>
+
+      <div className="w-full flex flex-row gap-2 items-center grow text-xl">
+        <Separator className="shrink" />
+        OR
+        <Separator className="shrink" />
+      </div>
+
+      <Button
+        variant="outline"
+        className="flex items-center justify-start gap-4 w-full px-4 text-base font-normal
+        "
+        size="lg"
+        onClick={() => signUpWithGoogle()}
+      >
+        <GoogleIcon size={20} /> Continue with Google
+      </Button>
+
+      <Link href="/login">
         <Button
-          disabled={form.formState.isSubmitting}
-          className="flex items-center gap-x-2"
-          onClick={form.handleSubmit(onSubmit)}
+          variant="link"
+          className="flex items-end p-0"
         >
-          Register
-          {form.formState.isSubmitting && (
-            <LoaderCircleIcon className="animate-spin" />
-          )}
+          {"Already have an account?"}
         </Button>
-      </CardFooter>
-    </Card>
+      </Link>
+    </section>
   );
 }
