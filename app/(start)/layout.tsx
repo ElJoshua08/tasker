ed15201a233de46c6bead14f90216d533bd93714
ElJoshua08 <josuealejandrof926@gmail.com>
2024-10-20 15:59:08 +0200

    IDK

--- app/(start)/layout.tsx
+++ app/(start)/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
   ] as NavItem[]
 
   return (
-    <main className="p-4 flex flex-col min-h-screen w-full justify-start gap-2">
+    <main className="p-4 flex flex-col min-h-screen w-full justify-start gap-2 overflow-hidden">
+      <div className="overflow-hidden absolute top-0 left-0 -z-10 h-full w-full bg-background">
+        <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[60%] rounded-full bg-[rgba(211,198,226,0.5)] dark:bg-[rgba(37,37,37,0.82)] opacity-50 dark:blur-[60px] blur-[120px]" />
+      </div>
+
       <Navbar items={navItems} />
       <Separator className="mt-2" /> 
       {children}
