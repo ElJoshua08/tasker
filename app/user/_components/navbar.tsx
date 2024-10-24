50fde44e8e404ea6f43e0e0b75ae5721e017a6c3
ElJoshua08 <josuealejandrof926@gmail.com>
2024-10-24 14:11:48 +0200

    This should work

--- app/user/_components/navbar.tsx
+++ app/user/_components/navbar.tsx
@@ -41,7 +41,7 @@ export const Navbar = ({
       animate={{ width: isOpen ? WIDTHS.open : WIDTHS.closed }}
       transition={{ type: "spring", stiffness: 300, damping: 30 }}
     >
-      <header className={cn("flex flex-row gap-2 w-full items-center", isOpen ? "justify-start" : "justify-center")}>
+      <header className={cn("flex flex-row gap-2 w-full items-center justify-start", isOpen ? "w-full" : "size-10")}>
         <Avatar className="bg-primary/50 text-2xl text-foreground flex items-center justify-center font-semibold">
           {user.name.charAt(0)}
         </Avatar>
